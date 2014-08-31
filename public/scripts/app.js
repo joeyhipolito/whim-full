fac1444f6f6f9cb44277cc1357b77d25d4e73754
Joey Hipolito <hi@joeyhipolito.com>
2014-09-01 01:41:05 +0800

    init and scaffold whim backend frontend integration

---
+++ public/scripts/app.js
@@ -0,0 +1,23 @@
+{
+  "name": "whim",
+  "version": "0.0.5",
+  "dependencies": {
+    "angular": "1.2.16",
+    "json3": "~3.3.1",
+    "es5-shim": "~3.1.0",
+    "angular-resource": "1.2.16",
+    "angular-cookies": "1.2.16",
+    "angular-sanitize": "1.2.16",
+    "angular-animate": "1.2.16",
+    "angular-touch": "1.2.16",
+    "angular-route": "1.2.16",
+    "angular-ui-router": "~0.2.10",
+    "angular-ui-codemirror": "~0.1.6",
+    "ionic": "driftyco/ionic-bower#master"
+  },
+  "resolutions": {
+    "angular": "~1.2.17",
+    "angular-sanitize": "~1.2.17",
+    "angular-animate": "~1.2.17"
+  }
+}
