a0a961009d074d18533310687803fe33565ae2f0
Joey Hipolito <hi@joeyhipolito.com>
2014-09-11 13:15:28 +0800

    add run validation

--- controllers/container.js
+++ controllers/container.js
@@ -13,36 +13,54 @@ exports.query = function (req, res) {
 exports.run = function (req, res) {
   var cid = req.param('id');
   
-  docker.createContainer({'Image': 'whim/node'}, function (err, container) {
-    Container.findOne({'cid': cid}, function(err, dataContainer){
-      dataContainer.worker = {
-        id: container.id.substr(0,8),
-        status: 'running'
-      };
-      dataContainer.save();
-    });
-    res.json({worker: container.id});
-    container.attach({stream: true, stdout: true, stderr: true, tty: true}, function (err, stream) {
-      stream.pipe(process.stdout);
-      container.start({'VolumesFrom': cid, 'PublishAllPorts': true}, function (err, data) {
-        console.log(data);
+  Container.findOne({'cid': cid}, function(err, dataContainer){
+    if (err) {
+      res.json({
+        error: true,
+        message: err
       });
-    })
+    }
+    if (dataContainer) {
+      if (dataContainer.worker.status === 'running') {
+        res.json({error: true, message: 'The application is already running'});
+      } else {
+        docker.createContainer({'Image': 'whim/node'}, function (err, container) {
+
+          dataContainer.worker = {
+            id: container.id.substr(0,8),
+            status: 'running'
+          };
+          dataContainer.save();
+
+          res.json({worker: container.id});
+          container.attach({stream: true, stdout: true, stderr: true, tty: true}, function (err, stream) {
+            stream.pipe(process.stdout);
+            container.start({'VolumesFrom': cid, 'PublishAllPorts': true}, function (err, data) {
+              console.log(data);
+            });
+          });
+        });
+      }
+    }
   });
 };
 
 exports.read = function (req, res) {
   var cid = req.param('id');
 
-  var container = docker.getContainer(cid);
-  container.inspect(function(err, data){
+  docker.getContainer(cid).inspect(function(err, data){
     res.json(data);
   });
 };
 
 exports.stop = function (req, res) {
   var cid = req.param('id');
-  docker.getContainer(cid).stop(function(err, data){
-    res.json(data);
+
+  docker.getContainer(cid).stop(function(err){
+    if (err !== null) {
+      res.json({stopped: true});
+    } else {
+      res.json({stopped: false});
+    }
   });
 }
