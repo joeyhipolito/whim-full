a45c981f21e8c75e942dcdc311080ff5d0b035f9
Joey Hipolito <hi@joeyhipolito.com>
2014-09-11 11:08:04 +0800

    add container id to container schema

--- controllers/repo.js
+++ controllers/repo.js
@@ -20,6 +20,7 @@ exports.clone = function(req, res) {
       }, function (err, container) {
         container.start(function (err, data) {
           var newContainer = Container();
+          newContainer.cid  = container.id;
           newContainer.name = req.user.username + '-' + req.body.name;
           newContainer.user = req.user._id;
           newContainer.save(function(err){
--- models/container.js
+++ models/container.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 
 var containerSchema = mongoose.Schema({
+  cid           : String,
   name          : String,
   user          : String,
   status        : String,
+  worker        : String,
   createdAt     : { type: Date, default: Date.now }
 });
 
