9f8159248946694628a677ffb5b84ada1bd48515
Joey Hipolito <hi@joeyhipolito.com>
2014-09-18 12:37:59 +0800

    patch

--- controllers/container.js
+++ controllers/container.js
@@ -12,43 +12,79 @@ exports.query = function (req, res) {
 
 exports.run = function (req, res) {
   var dataContainerID = req.param('id');
-  
-  Container.findOne({'cid': dataContainerID}, function(err, dataContainer){
-    if (err) { res.json({ error: err })};
-    console.log(err);
+
+  Container.findOne({'cid': dataContainerID}, function (err, dataContainer) {
+    if (err) {res.json({error: err})};
     if (dataContainer) {
       if (dataContainer.app.status === 'running') {
-        console.log('already running');
-        res.json({error: 'The application is already running'});
+        res.json({error: 'app running...'});
       } else {
-        console.log('not created yet');
-        docker.createContainer({'Image': 'whim/npm:start'}, function (err, container) {
-          console.log(container);
-          dataContainer.app.id = container.id.substr(0,12);
+        docker.createContainer({Image: 'whim/npm:start'}, function (err, container) {
+          dataContainer.app.id = container.id.substr(0, 12);
           dataContainer.app.status = 'running';
-          dataContainer.save();
-          
-          container.attach({stream: true, stdout: true, stderr: true, tty: true}, function (err, stream) {
-            console.log('attaching');
-            stream.pipe(process.stdout);
-            container.start({'VolumesFrom': dataContainerID, 'PublishAllPorts': true}, function (err, data) {
-              console.log(data);
-            });
-
-            docker.getContainer(dataContainer.app.id).inspect(function (err, data) {
-              console.log('inspect data here');
-              console.log(data.NetworkSettings);
-              console.log(data.NetworkSettings.Ports['5000/tcp']);
-              dataContainer.app.port = data.NetworkSettings.Ports['5000/tcp'][0].HostPort;
-              dataContainer.save(function(err, data){
-                res.json(dataContainer);
+          dataContainer.save(function (err) {
+            container.attach({stream: true, stdout: true, stderr: true, tty: true}, function (err, stream) {
+              stream.pipe(process.stdout);
+              container.start({'VolumesFrom': dataContainerID, 'PublishAllPorts': true}, function (err, data) {
+                console.log(data);
+                console.log('inspect data here');
+                console.log(data.NetworkSettings);
               });
-            });
+
+              docker.getContainer(dataContainer.app.id).inspect(function (err, data) {
+                if (data.NetworkSettings.Ports['5000/tcp']) {
+                  console.log(data.NetworkSettings.Ports['5000/tcp']);;
+                  dataContainer.app.port = data.NetworkSettings.Ports['5000/tcp'][0].HostPort
+                  dataContainer.save();
+                };
+                res.json(dataContainer);
+              })
+
+            })
           });
         });
       }
-    }
-  });
+
+    };
+  })
+
+
+  // Container.findOne({'cid': dataContainerID}, function(err, dataContainer){
+  //   if (err) { res.json({ error: err })};
+  //   console.log(err);
+  //   if (dataContainer) {
+  //     if (dataContainer.app.status === 'running') {
+  //       console.log('already running');
+  //       res.json({error: 'The application is already running'});
+  //     } else {
+  //       console.log('not created yet');
+  //       docker.createContainer({'Image': 'whim/npm:start'}, function (err, container) {
+  //         console.log(container);
+  //         dataContainer.app.id = container.id.substr(0,12);
+  //         dataContainer.app.status = 'running';
+  //         dataContainer.save();
+          
+  //         container.attach({stream: true, stdout: true, stderr: true, tty: true}, function (err, stream) {
+  //           console.log('attaching');
+  //           stream.pipe(process.stdout);
+  //           container.start({'VolumesFrom': dataContainerID, 'PublishAllPorts': true}, function (err, data) {
+  //             console.log(data);
+  //           });
+
+  //           docker.getContainer(dataContainer.app.id).inspect(function (err, data) {
+  //             console.log('inspect data here');
+  //             console.log(data.NetworkSettings);
+  //             console.log(data.NetworkSettings.Ports['5000/tcp']);
+  //             dataContainer.app.port = data.NetworkSettings.Ports['5000/tcp'][0].HostPort;
+  //             dataContainer.save(function(err, data){
+  //               res.json(dataContainer);
+  //             });
+  //           });
+  //         });
+  //       });
+  //     }
+  //   }
+  // });
 };
 
 exports.read = function (req, res) {
